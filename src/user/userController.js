5df48e7b0b454564c743dae271547d1e7b20fffe
Nguyen_Huu_Thieng <16521532@gm.uit.edu.vn>
2023-09-08 14:00:39 +0700

    #7 - validator username

--- src/user/userController.js
+++ src/user/userController.js
@@ -2,6 +2,17 @@ const UserService = require('./userService')
 
 class UserController {
   register = async (req, res, next) => {
+    const { username } = req.body
+
+    if (!username) {
+      return res.status(400).json({
+        message: 'username null',
+        status: 400,
+        validationErrors: {
+          username: 'Username cannot be null'
+        }
+      })
+    }
     await UserService.save({ ...req.body })
     return res.status(200).json({
       message: 'User created',
