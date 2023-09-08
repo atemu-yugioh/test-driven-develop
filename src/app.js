ed9c2da77f0c813d2c029893d4664f0a499ee994
Nguyen_Huu_Thieng <16521532@gm.uit.edu.vn>
2023-09-08 09:47:15 +0700

    #1 done testing hashed password

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 const express = require('express')
 
 const app = express()
+const bcrypt = require('bcrypt')
 
 const userModel = require('./user/userModel')
 
 app.use(express.json())
 
 app.post('/api/v1/users', async (req, res, next) => {
-  const newUser = await userModel.create({ ...req.body })
+  const passwordHash = await bcrypt.hash(req.body.password, 10)
+  const newUser = await userModel.create({ ...req.body, password: passwordHash })
 
   if (newUser) {
     return res.status(200).json({
