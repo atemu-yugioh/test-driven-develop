9ac182100ca45d15227584a2e5242129eed860bc
Nguyen_Huu_Thieng <16521532@gm.uit.edu.vn>
2023-09-29 17:26:12 +0700

    #61 testing authorization jwt

--- src/auth/TokenService.js
+++ src/auth/TokenService.js
@@ -4,6 +4,10 @@ class TokenService {
   static createToken = async (user) => {
     return jwt.sign({ id: user.id }, 'this-is-secret-key')
   }
+
+  static verifyToken = async (token) => {
+    return await jwt.verify(token, 'this-is-secret-key')
+  }
 }
 
 module.exports = TokenService
